1fdcdfc61d8dcdb0b3176f2eb7bbe74a5b69f447
文若 <wankang@zorronline.com>
2022-06-07 16:47:36 +0800

    实现一个基础向量类

---
+++ main_vector.py
@@ -0,0 +1,9 @@
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+    lst = [5, 2, 1]
+    vec = Vector(lst)
+    print(vec)
+    print(len(vec))
+    for i in range(0, len(lst)):
+        print("vec[{}] = {}".format(i, vec[i]), end=" ")
