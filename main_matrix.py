723cc6629227eda1be4a367a8fcc1a9e577a78b5
wankcn <wankcn@icloud.com>
2022-06-19 21:20:48 +0800

    实现矩阵类

--- main_matrix.py
+++ main_matrix.py
@@ -5,3 +5,14 @@ from playLA.Matrix import Matrix
 if __name__ == "__main__":
     matrix = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     print(matrix)
+
+    print("matrix.shape = {}".format(matrix.shape()))
+    print("matrix.row = {}".format(matrix.row_num()))
+    print("matrix.col = {}".format(matrix.col_num()))
+    print("matrix.size = {}".format(matrix.size()))
+
+    # 获得某一行某一列的元素
+    print("matrix[0][2] = {}".format(matrix[0, 2]))
+
+    print(matrix.row_vector(0))
+    print(matrix.col_vector(1))  # 2,5,8
--- playLA/Matrix.py
+++ playLA/Matrix.py
@@ -1,8 +1,46 @@
 # Author: 文若
 # CreateDate: 2022/6/17
+from playLA.Vector import Vector
+
+
 class Matrix:
     def __init__(self, lst2d):
         self._values = [row[:] for row in lst2d]
+
+    def row_vector(self, index):
+        """获得矩阵第index个行向量"""
+        # 注意返回的是向量
+        return Vector(self._values[index])
+
+    def col_vector(self, index):
+        """获得矩阵第index个列向量"""
+        # 注意返回的是向量 取出每一行的第index个元素
+        return Vector(row[index] for row in self._values)
+
+    def __getitem__(self, pos):
+        """获得矩阵某一行某一列的元素 pos元组"""
+        r, c = pos
+        return self._values[r][c]
+
+    def size(self):
+        """返回矩阵元素的个数"""
+        r, c = self.shape()
+        return r * c
+
+    def row_num(self):
+        """返回矩阵的行数"""
+        return self.shape()[0]
+
+    """len返回矩阵的行"""
+    __len__ = row_num
+
+    def col_num(self):
+        """返回矩阵的列数"""
+        return self.shape()[1]
+
+    def shape(self):
+        """返回矩阵的形状：（行数，列数）"""
+        return len(self._values), len(self._values[0])
 
     def __repr__(self):
         return "Matrix({})".format(self._values)
